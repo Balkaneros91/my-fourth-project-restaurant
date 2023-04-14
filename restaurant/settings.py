110a8298054d2f7608b7b3dad3b99e938f90339a
Antonio-Claudio Andelic <andelic.antonio.claudio91@gmail.com>
2023-04-14 20:02:49 +0000

    Add correct host

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://my-fourth-project-restaurant.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['my-fourth-project-restaurant.herokuapp.com', 'localhost']
 
 
 # Application definition
